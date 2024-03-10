85a7ceb15e281eb9e1559f4caa6a23a828afea24
JoseLuis <jvazquez3@nigul.cide.es>
2024-03-10 19:01:15 +0100

    Updates

--- page/src/Home.tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "react-bootstrap";
-function Home(){
-    return(
-        <div className="d-flex justify-content-center">
-            <p className="text-light display-1"></p>
-        </div>
-    );
-}
-export default Home;
